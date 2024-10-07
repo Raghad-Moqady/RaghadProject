6130a2c716ed182058acb28aae2f5d44450774e8
Raghad-Moqady <raghadmoqady@gmail.com>
2024-10-08 00:33:18 +0300

    Add project files.

---
+++ RaghadProject.PL/Areas/Dashboard/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaghadProject.PL.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RaghadProject.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RaghadProject.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
